f56894102c9a2ae68134927d7c3ef3552bc979e1
jacob-amar <49690653+jacob-amar@users.noreply.github.com>
2019-04-16 13:23:41 -0400

    Update CFB Buylist Scrape.py

--- CFB Buylist Scrape.py
+++ CFB Buylist Scrape.py
@@ -17,7 +17,6 @@ for sets,code in standard_sets.items():
     for i in range(1,2):
         my_url = 'https://store.channelfireball.com/buylist/magic/'+code+'?filter%5B11%5D=&filter%5B255%5D=&filter%5B256%5D=Regular&filter%5B5%5D=&filter%5B8%5D=&filter_by_stock=&filtered=1&page=' + str(i) + '&sort_by_price=1'
         #opening connection, grabbing the page, closing connection
-        #print(my_url)
         uClient = uReq(my_url)
         page_html = uClient.read()
         uClient.close()
@@ -29,14 +28,13 @@ for sets,code in standard_sets.items():
         
         for container in products:
             subcontainer = container.find("div",{"class":"meta credit"})
-            cardName = container.div.a["title"]
+            card_name = container.div.a["title"]
             condition = subcontainer.div.div.span.span.text
             try: 
                 price = container.find("span",{"class":"regular price"}).text
             except:
                 continue
             
-            #print(cardName.replace(",","") + "," + condition + "," + price + "\n")
-            f.write(sets + "," + cardName.replace(",","") + "," + condition + "," + price + "\n")
+            f.write(sets + "," + card_name.replace(",","") + "," + condition + "," + price + "\n")
 
 f.close()
